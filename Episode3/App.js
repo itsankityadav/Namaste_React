84eaece8269c3a7ec9237fb24ec8adc5063c624d
itsankityadav <itsankityadav.ltd@gmail.com>
2024-02-26 01:48:02 +0530

    some updates

--- Episode3/App.js
+++ Episode3/App.js
@@ -1,6 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const heading = React.createElement("h1", {}, "This is my first react app");
+//
+const heading = <h1>Hello Akansha</h1>
+console.log(heading);
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(heading);
